92604c50a4d6a00597199bc25a7fd953cd65d273
AhmedKhaled-74 <ahmed.khaled.lalo@gmail.com>
2023-05-22 04:22:43 +0300

    01-continue

--- js/script.js
+++ js/script.js
@@ -4,16 +4,74 @@ const headerMenu = document.getElementById("main-header");
 const BM = document.getElementById("BM-menu");
 let clickCount = 0;
 
-btnMenu.addEventListener("click", MobNavMenu);
+/******************/
 
+// stiky nav bar.
+const ngr = document.querySelectorAll(".ng");
+const dash = document.querySelectorAll(".dsh");
+const sectionHeroEl = document.querySelector(".hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      // headerMenu.classList.add("fixed");
+      headerMenu.classList.add("backdrop-blur-sm");
+      headerMenu.classList.add("bg-veryDarkBlue");
+      headerMenu.classList.add("bg-opacity-95");
+      BM.classList.remove("fill-[#242A45]");
+      dash.forEach((element) => {
+        // do something with each element
+        element.classList.remove("bg-black");
+        element.classList.add("bg-white");
+      });
+      ngr.forEach((element) => {
+        // do something with each element
+        element.classList.remove("text-grayishBlue");
+        element.classList.add("text-gray-200");
+      });
+    }
+    if (ent.isIntersecting === true) {
+      // Check if the menu is closed (i.e., doesn't have the translate-x-0 class)
+      if (!menu.classList.contains("translate-x-0")) {
+        // headerMenu.classList.add("fixed");
+        headerMenu.classList.remove("backdrop-blur-sm");
+        headerMenu.classList.remove("bg-veryDarkBlue");
+        headerMenu.classList.remove("bg-opacity-95");
+        BM.classList.add("fill-[#242A45]");
+        dash.forEach((element) => {
+          // do something with each element
+          element.classList.add("bg-black");
+          element.classList.remove("bg-white");
+        });
+        ngr.forEach((element) => {
+          // do something with each element
+          element.classList.add("text-grayishBlue");
+          element.classList.remove("text-gray-200");
+        });
+      }
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0.99,
+  }
+);
+obs.observe(sectionHeroEl);
+
+/***************** */
 function MobNavMenu() {
   clickCount++;
   btnMenu.classList.toggle("open");
-  menu.classList.toggle("translate-x-full");
-  menu.classList.toggle("translate-x-0");
+
+  headerMenu.classList.toggle("backdrop-blur-sm");
   headerMenu.classList.toggle("bg-veryDarkBlue");
   headerMenu.classList.toggle("bg-opacity-95");
-  headerMenu.classList.toggle("backdrop-blur-sm");
+  menu.classList.toggle("translate-x-full");
+  menu.classList.toggle("translate-x-0");
+
   BM.classList.toggle("fill-[#242A45]");
 
   // Toggle the overflow-hidden class with a delay of 0 seconds on the first click
@@ -23,18 +81,57 @@ function MobNavMenu() {
   //   headerMenu.classList.toggle("overflow-hidden");
   // }, delayTime);
 
+  // const delayTime = clickCount === 1 ? 0 : 500;
+  // setTimeout(function () {
+  //   headerMenu.classList.toggle("container");
+  //   headerMenu.classList.toggle("mx-auto");
+  // }, delayTime);
+
   // // Reset the click count to loop the behavior
   // if (clickCount >= 2) {
   //   clickCount = 0;
   // }
 }
 
+btnMenu.addEventListener("click", () => {
+  let hasBgWhite = false;
+  dash.forEach((element) => {
+    if (element.classList.contains("bg-white")) {
+      hasBgWhite = true;
+    }
+  });
+
+  if (hasBgWhite) {
+    // At least one dash element has the bg-white class, so don't change the headerMenu background
+    btnMenu.classList.toggle("open");
+    menu.classList.toggle("translate-x-full");
+    menu.classList.toggle("translate-x-0");
+  } else {
+    // All dash elements don't have the bg-white class, so apply the full MobNavMenu() function
+    MobNavMenu();
+  }
+});
 // apply the function to all nav links when click
 
 const menuLinks = document.querySelectorAll(".section-btn");
 menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    MobNavMenu();
+    let hasBgWhite = false;
+    dash.forEach((element) => {
+      if (element.classList.contains("bg-white")) {
+        hasBgWhite = true;
+      }
+    });
+
+    if (hasBgWhite) {
+      // At least one dash element has the bg-white class, so don't change the headerMenu background
+      btnMenu.classList.toggle("open");
+      menu.classList.toggle("translate-x-full");
+      menu.classList.toggle("translate-x-0");
+    } else {
+      // All dash elements don't have the bg-white class, so apply the full MobNavMenu() function
+      MobNavMenu();
+    }
   });
 });
 /*The minWidth constant is calculated by multiplying the equivalent pixel width of 48em by the font size of the html element. The browser window width is then compared to minWidth, and if it is greater than minWidth
