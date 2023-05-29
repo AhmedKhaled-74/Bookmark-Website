8ed57a27f86d974d3285e0d0a375c8f9f090cb4b
AhmedKhaled-74 <ahmed.khaled.lalo@gmail.com>
2023-05-29 21:39:30 +0300

    ver3

--- js/script.js
+++ js/script.js
@@ -203,3 +203,29 @@ window.addEventListener("resize", () => {
     MobNavMenu();
   }
 });
+
+/************ */
+
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+// tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+function onTabClick(e) {
+  // deactivate all tabs
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      "border-b-4",
+      "border-softRed",
+      "md:border-b-0"
+    );
+  });
+  // hide all panels
+  panels.forEach((panel) => panel.classList.add("hidden"));
+  // activate a new tab and panel based on the target
+  e.target.classList.add("border-b-4", "border-softRed");
+  const classString = e.target.getAttribute("data-target");
+  document
+    .getElementById("panels")
+    .getElementsByClassName(classString)[0]
+    .classList.remove("hidden");
+}
